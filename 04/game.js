bd584a5c118abcf52207fce27fb23ef08e7fffaf
scsc3313 <scsc3313@naver.com>
2015-04-10 09:41:49 +0900

    04 과제

---
+++ 04/game.js
@@ -0,0 +1,28 @@
+
+var box = document.querySelectorAll('.box')[0];
+var x = 10;
+var y = 10;
+
+
+
+function moveBox(){
+
+  var valueX = Math.random() * 800 ;
+  var valueY =  Math.random() * 800;
+  box.style.left = valueX + 'px';
+  box.style.top = valueY + 'px';
+
+  x = valueX;
+  y = valueY;
+
+  if (x === 800 && y === 800){
+    x = 10;
+    y = 10;
+  }
+}
+
+
+var body = document.getElementById("body");
+body.addEventListener('click', startMove);
+
+function startMove(){ window.setInterval(moveBox, 1000); };
